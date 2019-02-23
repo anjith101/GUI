cbe564717e2aac4f3da59f5024745db4ff6fb8fb
anjith <anjithpadmanabhan@gmil.com>
2019-02-23 13:07:16 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GUI
 Creating a GUI with Kivy and python3.6
+
+this code shows you how to create a gui with kivy and python front end of this gui is devoloped using kivy lang and back end is controlled using python
---
+++ test.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_file("main.kv")
+
+# Declare both screens
+class StarterMenu(Screen):
+    pass
+
+class MycartMenu(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(StarterMenu(name='starter'))
+sm.add_widget(MycartMenu(name='mycart'))
+
+# Create the screen manager
+
+class TestApp(App):
+
+    def build(self):
+        return sm
+if __name__ =="__main__":
+
+    TestApp().run()
